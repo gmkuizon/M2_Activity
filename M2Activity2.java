d04e2963d6058003ac27564e21d65e0f081f022b
Gemmary M. Magallanes <gmmagallanes@headoffice.corp.bpi.com.ph>
2025-12-09 10:41:21 +0800

    M2 Activity 2

---
+++ M2Activity2.java
@@ -0,0 +1,12 @@
+package M2_Activity2;
+
+public class M2Activity2 {
+
+	public static void main(String[] args) {
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5,3);
+		
+		System.out.println("The total is: " + String.valueOf(sum));
+
+	}
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,11 @@
+package M2_Activity2;
+
+public class WhiteBoard {
+	
+	int addNumbers(int addend1, int addend2) {
+		
+		int sum = addend1 + addend2;
+		return sum;
+	}
+
+}
